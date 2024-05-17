bc3d1fea354b49286c3f80ed9e42999cbf060853
Mahesh192210312 <130142471+Mahesh192210312@users.noreply.github.com>
2024-05-17 10:00:45 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Circle
+{
+ public static void main(String ar[])
+ {
+   Scanner sc=new Scanner(System.in);
+   double A,r;
+   System.out.println("enter r: ");
+   r=sc.nextInt();
+   A=22.4*r*r;
+   System.out.println("AREA OF Circle :"+A);
+ }
+}
